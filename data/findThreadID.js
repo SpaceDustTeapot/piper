b1a48f26c954d0f6d1f642cd3554cfb9869be4e3
SpaceDustTeapot <jasonkarllees@hotmail.co.uk>
2015-12-11 13:51:43 +0000

    Now Recognises 'Valid' URLs eg. youtube

--- data/findThreadID.js
+++ data/findThreadID.js
@@ -5,7 +5,7 @@ exports.findThreadID = function (TabID)
  
  for(var i =0;i<Len;i++)
 {
-   TabID.substring(i,3);
+   TabID.substr(i,3);
 
 }
 
@@ -16,40 +16,56 @@ exports.WhatIsTab = function(TabID)
 console.log("INFUCKING FUNCTION");
 //0-not found, 1-8ch, 2-4chan 3-youtube
  var Len = TabID.length;
+ var temp;
+ var check;
  console.log("Len"+Len);
  for(var i =0;i<Len;i++)
  {
  console.log(i);
-  var temp = TabID.substring(i,3);
+  temp = TabID.substr(i,3);
   console.log(temp);
   if(temp == "com")
   {//assume youtube
-   var check = TabID.substring(12,6);
-  // console.log(check);
-   if(check == "youtube")
+   for(var k =0;k<Len;k++)
    {
-    console.log("Returning 3");
-    return 3;
-   }
-  }
+  	 var check = TabID.substr(k,7);
+  	 console.log(check);
+  	 console.log("in .com");
+  	 if(check == "youtube")
+   	{
+   	 console.log("in Youtube");
+   	 console.log("Returning 3");
+   	 return 3;
+  	 }
+   } 
+ }
   else if(temp == "org")
   {//assume 4ch
-    var check = TabID.substring(19,4);
-  //  console.log(check);
-    if(check == "4chan")
-    {
-	console.log("Returning 2");
-	return 2;
-    }
-  }
+    for(var k=0;k<Len;k++)
+    {	
+	 var check = TabID.substr(k,5);
+   	 console.log("in org");
+    	 console.log(check);
+   	 if(check == "4chan")
+   	 {
+		console.log("Returning 2");
+		return 2;
+   	 }
+    } 
+  } 
   else if(temp == "net")
   {//assume 8ch
-    var check = TabID.substring(9,3);
-    console.log(check);
-    if(check == "8ch")
+    for(var k=0; k<Len;k++)
     {
-	console.log("Returning 1");
-	return 1;
+
+   	 check = TabID.substr(k,3);
+   	 console.log(check);
+   	 console.log("in net");
+   	 if(check == "8ch")
+	 {
+		console.log("Returning 1");
+		return 1;
+   	 }
     }
   }
 
